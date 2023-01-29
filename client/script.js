e5e926cef1c267e525c4eeb7f87506590cc11700
itarek99 <tarekulislam7060@gmail.com>
2023-01-29 22:17:08 +0600

    complete frontend and backend

--- client/script.js
+++ client/script.js
@@ -61,6 +61,28 @@ const handleSubmit = async (e) => {
   chatContainer.scrollTop = chatContainer.scrollHeight;
   const messageDiv = document.getElementById(uniqueId);
   loader(messageDiv);
+
+  const response = await fetch('http://localhost:5000/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ prompt: data.get('prompt') }),
+  });
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = '';
+
+  if (response.ok) {
+    const data = await response.json();
+    const parseData = data.bot.trim();
+
+    typeText(messageDiv, parseData);
+  } else {
+    const err = await response.text();
+    messageDiv.innerHTML = 'Something Went Wrong.';
+    alert(err);
+  }
 };
 
 form.addEventListener('submit', handleSubmit);
